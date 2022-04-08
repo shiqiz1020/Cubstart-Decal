38341babd48f2324d0efec4096875f674d49e0e0
Shiqi Zhang <shiqiz1020@gmail.com>
2022-04-07 18:44:40 -0700

    finished

---
+++ hw07/Article.swift
@@ -0,0 +1,30 @@
+//
+//  Article.swift
+//  newsfeedSkeleton
+//
+//  Created by Shiqi Zhang on 2022/4/7.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct ArticleResponse: Codable {
+    let articles: [Article]
+}
+
+// MARK: - Article
+struct Article: Codable, Identifiable {
+    let id: UUID = UUID()
+    let author: String?
+    let url: String
+    let source, title: String
+    let articleDescription: String?
+    let image: String?
+    let date: Date
+
+    enum CodingKeys: String, CodingKey {
+        case author, url, source, title
+        case articleDescription = "description"
+        case image, date
+    }
+}
